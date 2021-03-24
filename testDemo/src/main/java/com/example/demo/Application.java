ed516a4d170a9a648881fa9ef1911023d19ce625
shiboyuan@casicyber.com <shiboyuan@casicyber.com>
2021-03-24 11:00:22 +0800

    mq学习

--- testDemo/src/main/java/com/example/demo/Application.java
+++ testDemo/src/main/java/com/example/demo/Application.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class Application {
 
 	public static void main(String[] args) {
