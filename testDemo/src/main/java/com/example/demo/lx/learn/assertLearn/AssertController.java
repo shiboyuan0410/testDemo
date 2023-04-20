26e506ecc7e6e794eb20a36ca3448e3491a4357e
shiboyuan <shiboyuan0410@163.com>
2023-04-20 10:20:56 +0800

    [feat]: asssert学习 solution:

---
+++ testDemo/src/main/java/com/example/demo/lx/learn/assertLearn/AssertController.java
@@ -0,0 +1,15 @@
+package com.example.demo.lx.learn.assertLearn;
+
+import org.springframework.util.Assert;
+
+public class AssertController {
+
+    public void cs(){
+        cs2();
+    }
+
+
+    public void cs2(){
+
+    }
+}
