ba2731438e6529f003b561a9ee880a6b37445c83
shiboyuan <shiboyuan0410@163.com>
2023-04-21 11:59:57 +0800

    [feat]: 功能整理 solution:

--- testDemo/src/main/java/com/example/demo/Application.java
+++ testDemo/src/main/java/com/example/demo/Application.java
@@ -1,12 +1,10 @@
 package com.example.demo;
 
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableRabbit
 public class Application {
 
 	public static void main(String[] args) {
